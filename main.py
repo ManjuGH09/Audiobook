069a5fb5ec09ce163a50c1b8013bbf28cc2cbe89
Manjunath H <61911337+ManjuGH09@users.noreply.github.com>
2020-12-01 11:36:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ book = open('adatb.pdf', 'rb')                  # addatb.pdf Is PDF File Name
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 speaker = pyttsx3.init()
-for num in range(29, pages):                    
+for num in range(29, pages):                    # Starting From Page Number 29 To Last Page
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
